c731c02aff897c634c07fe9ceddab1fbd1587084
Mark Stock <markjstock@gmail.com>
2020-11-05 15:33:19 -0500

    Fixed bugs introduced by stdint, getline

--- src/read_MSH_Mesh.cpp
+++ src/read_MSH_Mesh.cpp
@@ -19,6 +19,45 @@ namespace ReadMsh {
 std::vector< std::vector<uint32_t>> edge_type_node_number{ {1,8,26,27,28}, {2,3,4,5,6} }; //holds the number of nodes on each edge type
 std::vector< std::vector<uint32_t>> face_type_node_number{ {3,10,16,36,37}, {4,9,8,16,25} }; //holds the number of nodes on each 2D element type
 
+//
+// Non-class helper function for line-reading
+// Because getline works only on text files from the OS that it was compiled on...
+//
+// From: https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
+//
+std::istream& safeGetline(std::istream& is, std::string& t)
+{
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for(;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+        case '\n':
+            return is;
+        case '\r':
+            if(sb->sgetc() == '\n')
+                sb->sbumpc();
+            return is;
+        case std::streambuf::traits_type::eof():
+            // Also handle the case when the last line has no line ending
+            if(t.empty())
+                is.setstate(std::ios::eofbit);
+            return is;
+        default:
+            t += (char)c;
+        }
+    }
+}
+
 
 int32_t Mesh::read_msh_file (const char* const filename) {
 	// reads a msh file output from the Gmsh software. The msh file is in ASCII 4.1 version of the Gmsh output
@@ -27,8 +66,7 @@ int32_t Mesh::read_msh_file (const char* const filename) {
 	uint32_t index;
 	uint32_t nodes_min_index, nodes_max_index, raw_N_nodes, tag_N_nodes, nodes_total_entities, group_tag, entity_dim, unorganized_node_index = 0;
 	uint32_t elements_min_index, elements_max_index, tag_N_elements, elements_total_entities;
-    uint8_t element_type;
-	uint32_t N_boundary, node_index;
+	uint32_t element_type, N_boundary, node_index;
 	uint32_t entities_N_points, entities_N_curves, entities_N_faces;
 	double double_field;
 	double coorZ = 0.0;
@@ -56,11 +94,15 @@ int32_t Mesh::read_msh_file (const char* const filename) {
 		std::cout << "     Version is " << version << " and type is " << (filetype ? "binary" : "ASCII") << std::endl;
 		assert(version>=4.1 && "Error: Cannot read file versions less than 4.1");
 		assert(filetype==0 && "Error: Cannot read binary format files");
+	} else {
+		return 10;
 	}
 
 	// *************** Now read in the Boundaries field: locate the keyword $PhysicalNames *****************
 	//-----------------------------------------------------------------------------------------
 	found = locate_in_file(mshfile, "$PhysicalNames");
+	if (!found) return 11;
+
 	mshfile >> N_boundary;
 	boundaries.resize(N_boundary);
 	std::vector<uint32_t> tmp_boundary_tag(N_boundary); //temporary vector to store the boundary indices in MSH file (it is often irregular)
@@ -73,28 +115,38 @@ int32_t Mesh::read_msh_file (const char* const filename) {
 	// *************** Now read in the entities field: locate the keyword $Entities *****************
 	//-----------------------------------------------------------------------------------------
 	found = locate_in_file(mshfile, "$Entities");
+	if (!found) return 12;
+
 	mshfile >> entities_N_points >> entities_N_curves >> entities_N_faces >> tmp; //tmp is number of vols which is 0 for 2D problem
+	//std::cout << "Np " << entities_N_points << "  Nc " << entities_N_curves << "  Nf " << entities_N_faces << std::endl;
+
 	assert(!tmp); //double check that there is no volume in the domain, so 2D problem
-	
+
+	// skip the points
 	for (size_t i=0; i<entities_N_points; ++i) //skip the points
 		do
-			getline(mshfile, temp_string);
+			safeGetline(mshfile, temp_string);
 		while (temp_string.length() == 0);
 
-
+	// read in the curves
 	std::vector<uint32_t> tmp_curve_entity_tag(entities_N_curves); //temporary vector to store the curves entity tag in MSH file
 	std::vector<uint32_t> tmp_curves_boundary_tag(entities_N_curves); //writes the boundry tag of curves
 	for (size_t i = 0; i < entities_N_curves; ++i) { //read in the curves entity tag and the boundry tag
 		mshfile >> tmp_curve_entity_tag[i] >> double_field >> double_field >> double_field >> double_field >> double_field >> double_field
 			>> tmp >> tmp_curves_boundary_tag[i];
-		getline(mshfile, temp_string); //read in the rst of data which are not needed at this point
+		safeGetline(mshfile, temp_string); //read in the rst of data which are not needed at this point
 	}
+
+	// do not read in the faces
 
 	// *************** Now read in the NODES field: locate the keyword $Nodes *****************
 	//-----------------------------------------------------------------------------------------
 	found = locate_in_file(mshfile, "$Nodes");
+	if (!found) return 13;
 
 	mshfile >> nodes_total_entities >> raw_N_nodes >> nodes_min_index >> nodes_max_index;
+	//std::cout << "nte " << nodes_total_entities << "  Ne " << raw_N_nodes << std::endl;
+
 	unorganized_nodes_coor.resize(raw_N_nodes);
 	unorganized_node_type.resize(raw_N_nodes);
 	tmp = nodes_max_index - nodes_min_index + 1;
@@ -122,12 +174,18 @@ int32_t Mesh::read_msh_file (const char* const filename) {
 	uint32_t edge_index=0; //to store in the boundaries, the edges that form each boundary curve
 	//std::vector<std::vector<uint32_t>> tmp_curves_edges_tag(entities_N_curves); //store the tags for the edges forming each curve (curve is an entity, edgeis discretized form of curves)
 	found = locate_in_file(mshfile, "$Elements");
+	if (!found) return 14;
 
 	mshfile >> elements_total_entities >> N_elements >> elements_min_index >> elements_max_index; //N_element= total number of nodes, edges and 2d elements, ignore the 0d elements
+	//std::cout << "ete " << elements_total_entities << "  Ne " << N_elements << std::endl;
+	//std::cout << "Np " << entities_N_points << "  Nc " << entities_N_curves << "  Nf " << entities_N_faces << std::endl;
 	assert(elements_total_entities == entities_N_points + entities_N_curves + entities_N_faces);
+
 	for (uint32_t element_entity = 0; element_entity < elements_total_entities; ++element_entity) {
 		mshfile >> entity_dim >> group_tag; ////entity_dim=0(0d), 1(1d) , 2(2d) features; group_tag: tag of entity
 		mshfile >> element_type >> tag_N_elements; //1,8,26,27,28: 2-node, 3-node, 4-node, 5-node and 6-node lines; 3,10,16,36,37: 4-node, 9-node, 8-node, 16-node, 25-node 2D elements
+		//std::cout << "  dim " << entity_dim << "  tag " << group_tag << "  type " << element_type << "  num " << tag_N_elements << std::endl;
+
 		if (entity_dim==0 /*element_type==15*/)  //single-node point
 			for (uint32_t element = 0; element < tag_N_elements; ++element) //skip the nodes definitions
 				mshfile >> tmp1 >> tmp2;
@@ -191,6 +249,7 @@ int32_t Mesh::read_msh_file (const char* const filename) {
 			}
 		}
 	}
+	std::cout << "     Read " << elements_total_entities << " total entities" << std::endl;
 
 	// Now that the edge and elements are stored, the nodes constituting them should be renumbered
 	// use only the nodes that are used in the edges and elements vectors
@@ -230,63 +289,29 @@ int32_t Mesh::read_msh_file (const char* const filename) {
 		}
 	}
 
-	std::cout << "         Done" << std::endl;
+	std::cout << "     Done." << std::endl;
 	mshfile.close();
 
 	return retval;
 }
 
-//
-// Non-class helper function for line-reading
-// Because getline works only on text files from the OS that it was compiled on...
-//
-// From: https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
-//
-std::istream& safeGetline(std::istream& is, std::string& t)
-{
-    t.clear();
-
-    // The characters in the stream are read one-by-one using a std::streambuf.
-    // That is faster than reading them one-by-one using the std::istream.
-    // Code that uses streambuf this way must be guarded by a sentry object.
-    // The sentry object performs various tasks,
-    // such as thread synchronization and updating the stream state.
-
-    std::istream::sentry se(is, true);
-    std::streambuf* sb = is.rdbuf();
-
-    for(;;) {
-        int c = sb->sbumpc();
-        switch (c) {
-        case '\n':
-            return is;
-        case '\r':
-            if(sb->sgetc() == '\n')
-                sb->sbumpc();
-            return is;
-        case std::streambuf::traits_type::eof():
-            // Also handle the case when the last line has no line ending
-            if(t.empty())
-                is.setstate(std::ios::eofbit);
-            return is;
-        default:
-            t += (char)c;
-        }
-    }
-}
-
 bool Mesh::locate_in_file(std::ifstream& filestream, const std::string& searchname) {
 	//to find a specific keyword in the MSH file and return the file stream
+	//std::cout << std::endl << "searching for (" << searchname << ")" << std::endl;
+
 	std::string temp;
 	bool found = false;
+
 	while (!filestream.eof()) {
 		safeGetline(filestream, temp);
+		//std::cout << "line (" << temp << ")" << std::endl;
 		found = false;
 		if (temp == searchname) {
 			found = true;
 			break;
 		}
 	}
+
 	if (!found) std::cout << "The  " << searchname << "  Field Not Found! " << std::endl;
 	return found;
 }
--- src/read_MSH_Mesh.h
+++ src/read_MSH_Mesh.h
@@ -18,17 +18,20 @@ class Mesh {
 private:
 	uint32_t N_nodes = 0;     //# of nodes read from msh file
 	uint32_t N_elements = 0;  //# of elements read from msh file
-	std::vector<node> nodes;  //coordinates of the nodes
+	std::vector<node> nodes;  // coordinates of the nodes
 	std::vector<edge> edges;	 // types and nodes constituting edges of elements
 	std::vector<element2d> elements; // types and nodes constituting elements
-	std::vector<boundary> boundaries; // the boundaries of the problem, including the names and the dges that form each boundary
+	std::vector<boundary> boundaries; // the boundaries of the problem, including the names and the edges that form each boundary
 
 public:
-	Mesh() {} //default constructor
+	Mesh() {}	//default constructor
 	~Mesh() {}	//destructor define here later
 
+	// read the given mesh file and populate the data structures
 	int32_t read_msh_file(const char* const filename);
-	bool locate_in_file(std::ifstream& filestream, const std::string& searchname); //to find a searchname in the MSH file and return the file stream
+
+	// find a searchname in the MSH file and return the file stream after that line
+	bool locate_in_file(std::ifstream& filestream, const std::string& searchname);
 };
 
 }
